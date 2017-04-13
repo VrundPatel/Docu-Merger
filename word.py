d3cf9f603442c37aeeacdc3610898bbd23871f4a
Vrund Patel <vrundp07@yahoo.com>
2017-04-13 14:20:22 -0400

    Able to merge multiple docs

--- word.py
+++ word.py
@@ -1,14 +1,12 @@
 import os
-
 from docx import Document
-from docx.shared import Inches
 import getFullDocText as fullDoc
 
 finalDoc = Document()
-
-for file in os.listdir('docs'):
-    if file.endswith('.docx'):
-        fullText = fullDoc.getText(os.path.join('docs', file))
-        finalDoc.add_paragraph(fullText)
-
+for root, dirs, files in os.walk(os.getcwd()):
+    for name in files:
+        if 'Questions' in name and name.endswith('.docx'):
+            fullText = fullDoc.getText(os.path.join(root, name))
+            finalDoc.add_heading(name[:-5])
+            finalDoc.add_paragraph(fullText)
 finalDoc.save('docs/final.docx')
