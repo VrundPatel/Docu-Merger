8e578f1d6a9c630a0ec9588deb35b1ee7b239d90
Vrund Patel <vrundp07@yahoo.com>
2017-04-13 22:14:50 -0400

    Refactored code.

--- word.py
+++ word.py
@@ -13,18 +13,19 @@ def merge():
     finalStoriesDoc = Document()
     for root, dirs, files in os.walk(os.getcwd()):
         for name in files:
-            if 'Questions' in name and name.endswith('.docx') and 'final' not in name:
-                fullText = fullDoc.getText(os.path.join(root, name))
-                finalQuestionsDoc.add_heading(name[:-5])
-                finalQuestionsDoc.add_paragraph(fullText)
-            if 'Quotes' in name and name.endswith('.docx') and 'final' not in name:
-                fullText = fullDoc.getText(os.path.join(root, name))
-                finalQuotesDoc.add_heading(name[:-5])
-                finalQuotesDoc.add_paragraph(fullText)
-            if 'Stories' in name and name.endswith('.docx') and 'final' not in name:
-                fullText = fullDoc.getText(os.path.join(root, name))
-                finalStoriesDoc.add_heading(name[:-5])
-                finalStoriesDoc.add_paragraph(fullText)
+            if name.endswith('.docx') and 'final' not in name:
+                if 'Questions' in name:
+                    fullText = fullDoc.getText(os.path.join(root, name))
+                    finalQuestionsDoc.add_heading(name[:-5])
+                    finalQuestionsDoc.add_paragraph(fullText)
+                if 'Quotes' in name:
+                    fullText = fullDoc.getText(os.path.join(root, name))
+                    finalQuotesDoc.add_heading(name[:-5])
+                    finalQuotesDoc.add_paragraph(fullText)
+                if 'Stories' in name:
+                    fullText = fullDoc.getText(os.path.join(root, name))
+                    finalStoriesDoc.add_heading(name[:-5])
+                    finalStoriesDoc.add_paragraph(fullText)
     if not os.path.exists('final_docs'):
         os.makedirs('final_docs')
     finalQuestionsDoc.save('final_docs/finalQuestions.docx')
