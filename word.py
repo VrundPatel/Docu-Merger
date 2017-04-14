51d7047cb7e837c9f2153c48e596e48f13b0f4fc
Vrund Patel <vrundp07@yahoo.com>
2017-04-13 21:54:45 -0400

    Added GUI

--- word.py
+++ word.py
@@ -1,12 +1,27 @@
+# Import for working with file-system.
 import os
+# Import for the GUI
+import Tkinter as tk
+import tkMessageBox
+# Import for working with word documents
 from docx import Document
 import getFullDocText as fullDoc
 
-finalDoc = Document()
-for root, dirs, files in os.walk(os.getcwd()):
-    for name in files:
-        if 'Questions' in name and name.endswith('.docx'):
-            fullText = fullDoc.getText(os.path.join(root, name))
-            finalDoc.add_heading(name[:-5])
-            finalDoc.add_paragraph(fullText)
-finalDoc.save('docs/final.docx')
+def merge():
+    finalDoc = Document()
+    for root, dirs, files in os.walk(os.getcwd()):
+        for name in files:
+            if 'Questions' in name and name.endswith('.docx'):
+                fullText = fullDoc.getText(os.path.join(root, name))
+                finalDoc.add_heading(name[:-5])
+                finalDoc.add_paragraph(fullText)
+    finalDoc.save('docs/final.docx')
+    tkMessageBox.showinfo("Docu-Merger", "Documents merged!")
+
+window = tk.Tk()
+window.wm_title('Docu-Merger')
+window.geometry('200x100')
+window.configure(background='#146eff')
+button = tk.Button(window, text="Merge Document", command = merge)
+button.pack()
+window.mainloop()
