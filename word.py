153f05ce3e9451dfeab5531a60fa7cac78e41584
Vrund Patel <vrundp07@yahoo.com>
2017-04-14 23:57:08 -0400

    Ability to run from another folder.

--- word.py
+++ word.py
@@ -12,32 +12,44 @@ def merge():
     finalQuotesDoc = Document()
     finalStoriesDoc = Document()
     for root, dirs, files in os.walk(os.getcwd()):
-        for name in files:
-            if name.endswith('.docx') and 'final' not in name:
-                if 'Questions' in name:
-                    fullText = fullDoc.getText(os.path.join(root, name))
-                    finalQuestionsDoc.add_heading(name[:-5])
-                    finalQuestionsDoc.add_paragraph(fullText)
-                if 'Quotes' in name:
-                    fullText = fullDoc.getText(os.path.join(root, name))
-                    finalQuotesDoc.add_heading(name[:-5])
-                    finalQuotesDoc.add_paragraph(fullText)
-                if 'Stories' in name:
-                    fullText = fullDoc.getText(os.path.join(root, name))
-                    finalStoriesDoc.add_heading(name[:-5])
-                    finalStoriesDoc.add_paragraph(fullText)
-    if not os.path.exists('final_docs'):
-        os.makedirs('final_docs')
-    finalQuestionsDoc.save('final_docs/finalQuestions.docx')
-    finalQuotesDoc.save('final_docs/finalQuotes.docx')
-    finalStoriesDoc.save('final_docs/finalStories.docx')
-    tkMessageBox.showinfo("Docu-Merger", "Documents merged!")
-    window.destroy()
+        for d in dirs:
+            if 'Shiv' in d:
+                os.chdir(os.path.abspath(d))
+                for root, dirs, files in os.walk(os.getcwd()):
+                    for name in files:
+                        print('Name: ', name)
+                        # print('name', file)
+                        if name.endswith('.docx') and 'final' not in name:
+                            if 'Questions' in name:
+                                fullText = fullDoc.getText(os.path.join(root, name))
+                                finalQuestionsDoc.add_heading(name[:-5])
+                                finalQuestionsDoc.add_paragraph(name)
+                            if 'Quotes' in name:
+                                fullText = fullDoc.getText(os.path.join(root, name))
+                                finalQuotesDoc.add_heading(name[:-5])
+                                finalQuotesDoc.add_paragraph(fullText)
+                            if 'Stories' in name:
+                                fullText = fullDoc.getText(os.path.join(root, name))
+                                finalStoriesDoc.add_heading(name[:-5])
+                                finalStoriesDoc.add_paragraph(fullText)
+                # current = os.getcwd() + '/' + d;
+                print(os.getcwd())
+                final_docs_folder = os.getcwd() + '/final_docs/'
+                if not os.path.exists(final_docs_folder):
+                    # os.chdir(current)
+                    os.makedirs('final_docs', mode=0777)
+                finalQuestionsDoc.save(final_docs_folder + 'finalQuestions.docx')
+                finalQuotesDoc.save(final_docs_folder + 'finalQuotes.docx')
+                finalStoriesDoc.save(final_docs_folder + 'finalStories.docx')
+#     tkMessageBox.showinfo("Docu-Merger", "Documents merged!")
+#     window.destroy()
 
-window = tk.Tk()
-window.wm_title('Docu-Merger')
-window.geometry('200x100')
-window.configure(background='#146eff')
-button = tk.Button(window, text="Merge Document", command = merge)
-button.pack()
-window.mainloop()
+# window = tk.Tk()
+# window.wm_title('Docu-Merger')
+# window.geometry('200x100')
+# window.configure(background='#146eff')
+# button = tk.Button(window, text="Merge Document", command = merge)
+# button.pack()
+# window.mainloop()
+
+merge()
